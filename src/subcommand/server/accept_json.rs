72b670dd407742302e7415d951d429ce652c3979
Binge <zhiying8710@hotmail.com>
2024-04-02 23:17:08 +0800

    fuck casey

--- src/subcommand/server/accept_json.rs
+++ src/subcommand/server/accept_json.rs
@@ -14,19 +14,20 @@ where
     parts: &mut http::request::Parts,
     state: &S,
   ) -> Result<Self, Self::Rejection> {
-    let state = Arc::from_ref(state);
-    let json_api_enabled = true;
-    let json_header = parts
-      .headers
-      .get("accept")
-      .map(|value| value == "application/json")
-      .unwrap_or_default();
-    if json_header && json_api_enabled {
-      Ok(Self(true))
-    } else if json_header && !json_api_enabled {
-      Err((StatusCode::NOT_ACCEPTABLE, "JSON API disabled"))
-    } else {
-      Ok(Self(false))
-    }
+    Ok(Self(true))
+    // let state = Arc::from_ref(state);
+    // let json_api_enabled = true;
+    // let json_header = parts
+    //   .headers
+    //   .get("accept")
+    //   .map(|value| value == "application/json")
+    //   .unwrap_or_default();
+    // if json_header && json_api_enabled {
+    //   Ok(Self(true))
+    // } else if json_header && !json_api_enabled {
+    //   Err((StatusCode::NOT_ACCEPTABLE, "JSON API disabled"))
+    // } else {
+    //   Ok(Self(false))
+    // }
   }
 }
